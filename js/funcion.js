8374407809498d78409d41ba76bd2e231dad573b
jhontb <50501460+jhontb@users.noreply.github.com>
2019-05-10 15:40:22 -0500

    Add files via upload

---
+++ js/funcion.js
@@ -0,0 +1,19 @@
+$(function () {
+	$('#form-search').submit(function(e) {
+		e.preventDefault();
+	})
+	$('#buscar').keyup(function(){
+		var envio= $('#buscar').val();
+		$('#resultado-q').html('<h4>Cargando</h4>');
+		$.ajax({
+			type: 'post',
+			url: '../clases/buscar.php',
+			data: ('buscar='+envio),
+			success: function(respuesta) {
+				if(respuesta != '') {
+					$('#resultado-q').html(respuesta);
+				}
+			}
+		})
+	})
+})
